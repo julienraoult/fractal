2831dec8aff1b1f89de23bb37440d462904bfc30
Julien Raoult <81713118+julienraoult@users.noreply.github.com>
2021-05-15 19:28:39 +0200

    Add files via upload

--- lyapunov.py
+++ lyapunov.py
@@ -2,6 +2,8 @@
                 
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
+import time
 
 def F_logistic(x, mu):
     return mu*x*(1-x)
@@ -31,22 +33,21 @@ class Lyapunov:
         self.b = self.b.T        
         
         # test = np.array([[0., 4.],[0., 4.]])
-        skip = 1200
-        #for i in range(skip):
-        #    seq  = self.a[0][0] if (i%2 == 0) else self.b[0][0]
-        #    pn   = F_logistic(pn, seq)                    
         exponent = np.zeros((step, step))
-        for index_a in range(step):
-            print(index_a)
-            for index_b in range(step):
-                pn  = 0.5
-                exp = 0
-                for i in range(skip, skip + self.nb_iters):
-                    seq  = self.a[0][index_a] if (i%2 == 0) else self.b[0][index_b]
-                    pn   = F_logistic(pn, seq)                                        
-                    exp += np.log(np.absolute(Fprime_logistic(pn, seq)))
-                exponent[index_a,index_b] = exp / self.nb_iters
-                # print(exp)
+        log = True
+        for i, j in itertools.product(range(step), range(step)):
+            pn  = 0.5
+            exp = 0
+            if log:
+                print(f'{i}', end='\r')
+                log = False
+            for k in range(self.nb_iters):
+                seq  = self.a[0][i] if (k%2 == 0) else self.b[0][j]
+                pn   = F_logistic(pn, seq)                                        
+                exp += np.log(np.absolute(Fprime_logistic(pn, seq)))
+            exponent[i, j] = exp / self.nb_iters
+            if (j==step-1): log = True
+            # print(exp)
         self.exponent = exponent
         #print(exponent)            
         return 
@@ -57,7 +58,7 @@ class Lyapunov:
         cf. http://matplotlib.org/examples/color/colormaps_reference.html
         '''
         cmaps = ['plasma', 'bwr', 'Spectral', 'gist_rainbow', 'gnuplot2', 'nipy_spectral']
-        my_cmap = plt.get_cmap(cmaps[0])
+        my_cmap = plt.get_cmap(cmaps[5])
         my_cmap.set_over('black')              # clamp value over vmax to this value
         my_cmap.set_under('#124585')           # clamp value under vmin to this value
 
@@ -70,15 +71,18 @@ class Lyapunov:
         plt.xlabel('a')
         plt.ylabel('b')                
         # https://matplotlib.org/stable/tutorials/intermediate/imshow_extent.html
-        plt.imshow(self.exponent, cmap = my_cmap, vmin = -2, vmax = 0.0001,
+        plt.imshow(self.exponent, cmap = my_cmap, vmin = -2, vmax = 0.01,
                    origin = 'lower', extent = extent)
         plt.colorbar()
         plt.show()
             
 def main():
     print('Lyapunov in python!')
-    lyapunov = Lyapunov(nb_iters = 200, ab_min = 2, ab_max = 4, ab_step = 0.004)
+    lyapunov = Lyapunov(nb_iters = 100, ab_min = 2, ab_max = 4, ab_step = 0.001)
+    t1 = time.time()
     lyapunov.process()
+    t2 = time.time()
+    print(f'Time taken by computing: {t2-t1}')
     lyapunov.display()
     
 if __name__ == "__main__":
